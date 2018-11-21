99bcd36ce4b8ed4e092b831ba95aed8144b046e9
Lon Ilesanmi <lonilesanmi@gmail.com>
2018-11-21 22:11:34 +0100

    Reformat code

--- longest_substring.go
+++ longest_substring.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 func lengthOfLongestSubstring(s string) int {
-    longest := ""
-    current := ""
+	longest := ""
+	current := ""
 
-    for _, v := range s {
+	for _, v := range s {
 		vStr := string(v)
 
 		// Slower
 		// split := strings.Split(current, vStr)
-        // if len(split) > 1 {
-		// 	fmt.Printf("\nFound %s, Resetting %s to: %s", vStr, current, split[1])
-        //     current = split[1]
-        // }
+		// if len(split) > 1 {
+		//     fmt.Printf("\nFound %s, Resetting %s to: %s", vStr, current, split[1])
+		//     current = split[1]
+		// }
 
 		// Faster
-        oldIndex := strings.Index(current, vStr)
-        if oldIndex > -1 {
-			fmt.Printf("\nFound %s at %d, Resetting %s to: %s", vStr, oldIndex, current, current[oldIndex + 1:])
-            current = current[(oldIndex + 1):]
-        }
+		oldIndex := strings.Index(current, vStr)
+		if oldIndex > -1 {
+			fmt.Printf("\nFound %s at %d, Resetting %s to: %s", vStr, oldIndex, current, current[oldIndex+1:])
+			current = current[(oldIndex + 1):]
+		}
 
-        current += string(vStr)
+		current += string(vStr)
 
-        if len(current) > len(longest) {
-            longest = current
-        }
-    }
+		if len(current) > len(longest) {
+			longest = current
+		}
+	}
 
 	fmt.Printf("\nLongest String: %s", longest)
-    return len(longest)
+	return len(longest)
 }
 
 func main() {
