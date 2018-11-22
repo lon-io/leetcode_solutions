b96ccae04923003155009903dc0325c6edada055
Lon Ilesanmi <lonilesanmi@gmail.com>
2018-11-22 01:17:55 +0100

    Add Sliding window solution for Longest substring

--- longest_substring.go
+++ longest_substring.go
@@ -12,21 +12,21 @@ func lengthOfLongestSubstring(s string) int {
 	for _, v := range s {
 		vStr := string(v)
 
-		// Slower
+		// Slower (20ms)
 		// split := strings.Split(current, vStr)
 		// if len(split) > 1 {
 		//     fmt.Printf("\nFound %s, Resetting %s to: %s", vStr, current, split[1])
 		//     current = split[1]
 		// }
 
-		// Faster
+		// Faster (12 ms)
 		oldIndex := strings.Index(current, vStr)
 		if oldIndex > -1 {
 			fmt.Printf("\nFound %s at %d, Resetting %s to: %s", vStr, oldIndex, current, current[oldIndex+1:])
 			current = current[(oldIndex + 1):]
 		}
 
-		current += string(vStr)
+		current += vStr
 
 		if len(current) > len(longest) {
 			longest = current
@@ -37,9 +37,47 @@ func lengthOfLongestSubstring(s string) int {
 	return len(longest)
 }
 
+// Slower (24 ms)
+// Possible explanation here: https://stackoverflow.com/a/29690063/4931825
+// String keys require hashing
+func lengthOfLongestSubstringSlidingWindow(s string) int {
+	soln := 0
+	seenMap := map[string]int{}
+	i := 0
+
+	for j, v := range s {
+		vStr := string(v)
+
+		k, ok := seenMap[vStr]
+		if ok && k >= i {
+			i = k + 1
+			fmt.Printf("\nFound %s at %d, Resetting i to: %d, with j == %d", vStr, k, i, j)
+		}
+
+		seenMap[vStr] = j
+		current := j - i + 1
+		if current > soln {
+			soln = current
+		}
+	}
+
+	return soln
+}
+
 func main() {
-	test1 := "pwwkew"
-	test2 := "dvdf"
+	test1 := "a"      // 1
+	test2 := "bbbbbb" // 1
+	test3 := "pwwkew" // 3
+	test4 := "dvdf"   // 3
 	fmt.Printf("\n%d", lengthOfLongestSubstring(test1))
 	fmt.Printf("\n%d", lengthOfLongestSubstring(test2))
+	fmt.Printf("\n%d", lengthOfLongestSubstring(test3))
+	fmt.Printf("\n%d", lengthOfLongestSubstring(test4))
+
+	fmt.Println("")
+
+	fmt.Printf("\n%d", lengthOfLongestSubstringSlidingWindow(test1))
+	fmt.Printf("\n%d", lengthOfLongestSubstringSlidingWindow(test2))
+	fmt.Printf("\n%d", lengthOfLongestSubstringSlidingWindow(test3))
+	fmt.Printf("\n%d", lengthOfLongestSubstringSlidingWindow(test4))
 }
